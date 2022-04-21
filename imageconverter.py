7b9b8e23923e6cfcdc6b7059599d60cd0c39f3dc
Ian Vazquez Perez <34981855+ianvazperez@users.noreply.github.com>
2022-04-21 16:25:09 +0100

    Added images to video converter and fixed bugs

--- imageconverter.py
+++ imageconverter.py
@@ -1,17 +1,19 @@
 import cv2
+import os
 
 vidcap = cv2.VideoCapture('video.mp4')
-
+directory = r'C:/Users/ianva/OneDrive/mdm2-rep4/images/'
 def getFrame(sec):
     vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
     hasFrames,image = vidcap.read()
     if hasFrames:
-        cv2.imwrite("image"+str(count)+".jpg", image)     # save frame as JPG file
+        os.chdir(directory)
+        cv2.imwrite("image "+str(count)+".jpg", image)
     return hasFrames
 
 sec = 0
 
-frameRate = 0.5 #//it will capture image in each 0.5 second
+frameRate = 1
 
 count=1
 
